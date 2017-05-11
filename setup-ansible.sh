e640c59389e730eb36c2bcb4a5096dcdd6596af3
ry <rgoodfel@isi.edu>
2017-05-10 20:41:20 -0700

    handle installing ansible for both debian and ubuntu

--- setup-ansible.sh
+++ setup-ansible.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
+dist=`lsb_release -i | awk '{print $3}'`
+
+if [[ "$dist" -eq "Debian" ]]; then
+	echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/ansible.list
+else
+	sudo apt-get install -y software-properties-common
+	sudo apt-add-repository -y ppa:ansible/ansible
+fi
+
 sudo apt-get update
 sudo apt-get install -y ansible
+exit
+
 
