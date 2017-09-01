b130d7b90b41ad8f296847293fffc72ecf22fe85
Ryan Goodfellow <rgoodfel@isi.edu>
2017-08-31 21:58:12 -0700

    no ask on install

--- setup-ansible.sh
+++ setup-ansible.sh
@@ -7,7 +7,7 @@ set -e
 dist=`lsb_release -i | awk '{print $3}'`
 
 if [[ "$dist" -eq "Debian" ]]; then
-  sudo apt install dirmngr
+  sudo apt install -y dirmngr
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
   echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/ansible.list
 else
