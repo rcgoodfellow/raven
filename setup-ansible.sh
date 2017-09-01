3095ff4672c242e996236178de2f476ae51fcf61
Ryan Goodfellow <rgoodfel@isi.edu>
2017-08-31 20:42:15 -0700

    fixes to setup process for debian stretch

--- setup-ansible.sh
+++ setup-ansible.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+set -x
+set -e
+
+
 dist=`lsb_release -i | awk '{print $3}'`
 
 if [[ "$dist" -eq "Debian" ]]; then
+  sudo apt install dirmngr
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-	echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/ansible.list
+  echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/ansible.list
 else
-	sudo apt-get install -y software-properties-common
-	sudo apt-add-repository -y ppa:ansible/ansible
+  sudo apt install -y software-properties-common
+  sudo apt-add-repository -y ppa:ansible/ansible
 fi
 
-sudo apt-get update
-sudo apt-get install -y ansible
+sudo apt update
+sudo apt install -y ansible
 exit
 
 
